61401abd7f5fb8bcabedbb9c276ec052aac16408
prajva-sudo <prajval.bhardwaj@skewb.ai>
2024-12-07 09:25:13 +0530

    add setup download in info-bar

--- src/components/global/info-bar/index.tsx
+++ src/components/global/info-bar/index.tsx
@@ -7,7 +7,8 @@ import React from 'react'
 
 
 const InfoBar = () => {
-  return (
+  console.log('env->',`${process.env.NEXT_PUBLIC_SETUP_DOWNLOAD}`)
+    return (
     <header className='pl-20 md:pl-[265px] fixed p-4 w-full flex items-center justify-end gap-4'>
         {/* <div className='  flex gap-4 justify-center items-center border-2 rounded-full px-4 w-full max-w-lg'>
             <Search size={25} className='text-[#707070]' />
@@ -22,7 +23,7 @@ const InfoBar = () => {
                <VideoRecorderIcon/>
                 <span>Record</span>
             </Button> */}
-            <a href={`${process.env.NEXT_PUBLIC_CLOUD_FRONT_STREAM_URL}/YourAppName-Windows-0.0.0-Setup.exe`}>
+            <a href={`${process.env.NEXT_PUBLIC_SETUP_DOWNLOAD}`}>
             <Button  className='bg-[#9D9D9D]'>
                 <Download size={20}/>
                 <span>Setup</span>
